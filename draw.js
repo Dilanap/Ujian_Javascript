1b2309d56a856e21c4a3717ffb2b882c863b1570
Dilanap <dilanapritasia@gmail.com>
2021-04-24 04:53:40 +0700

    update draw.js

--- draw.js
+++ draw.js
@@ -1,3 +1,4 @@
+
 class Draw{
  Kotak(x) {
     for (let i = 0; i < x; i++) {
@@ -36,19 +37,28 @@ for (let i = 0; i <a; i++){
   console.log("\n")
 }
 
-Kotakpola(x) {
-    for (let i = 0; i <a; i++){
-        for (let j = 0; j< a; j++) {
-          if ((i%2==0 && j%2==1) || (i%2==1 && j%2==0)){
-            process.stdout.write("*")
-          }else{
-            process.stdout.write("!")
-          }
-        }console.log(" ")
+
+Kotakpola(a, b) {
+  let jumlah = 0;
+  let hasil = "";
+  for (let x = 0; x < a; x++) {
+    jumlah++;
+    let countVar = 0;
+    for (let y = 0; y < b; y++) {
+      countVar++;
+      hasil += "*";
+      if (jumlah % 2 == y) {
+        hasil += "!";
       }
-      console.log("\n")
     }
+    hasil += "\n";
+  }
+
+  console.log(hasil);
 }
+}
+
+    
 let test=new Draw();
 test.Kotak(6);
 
@@ -62,4 +72,4 @@ let test3=new Draw();
 test3.Selangseling(5);
 
 let test4=new Draw();
-test4.Kotakpola(5);
+test4.Kotakpola(6,5);
